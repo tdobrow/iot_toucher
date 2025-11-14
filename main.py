e55374db1691bf219dccc81639695b9812953899
tdobrow <tomdobrow@gmail.com>
2025-11-14 12:28:24 -0600

    remove printing

--- main.py
+++ main.py
@@ -71,12 +71,6 @@ def handle_incoming(payload_bytes):
         sender = msg.get("client_id")
         action = msg.get("action")
 
-        print("TESTING HANDLE INCOMING")
-        print(msg)
-        print(sender)
-        print(action)
-        print(MY_ID)
-
         if action == "touch":
             if sender == MY_ID:
                 print("IT WAS MY MESSAGE")
@@ -90,16 +84,16 @@ def handle_incoming(payload_bytes):
 
 # awscrt callback signature; we ignore everything except payload
 def on_message(topic, payload, dup, qos, retain, **kwargs):
-    print("TESTING ON MESSAGE")
-    print(topic)
-    print(payload)
-
     handle_incoming(payload)
 
 def main():
     global MY_ID, STATE
     load_dotenv()
     topic = getenv("TOPIC")
+
+    # Signal that things are up and running
+    blink(GREEN_LED_PIN)
+    blink(GREEN_LED_PIN)
 
     while True:
         try:
