34524c32972e6f740fea5d3a82487f20494913ab
tdobrow <tomdobrow@gmail.com>
2026-01-09 09:12:46 -0600

    push

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ import time
 ROT_A_PIN = 17
 ROT_B_PIN = 23
 
+PUSH_PIN  = 14
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()  # clear any leftovers from previous runs
 
 GPIO.setup(ROT_A_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(ROT_B_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PUSH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def read_state():
-    state = GPIO.input(ROT_A_PIN), GPIO.input(ROT_B_PIN)
-    print("Rotary State: ${}".format(state))
-    return state
+    return GPIO.input(ROT_A_PIN), GPIO.input(ROT_B_PIN)
 
 def main():
     print("Polling rotary on A={}, B={} (BCM). Ctrl+C to exit.".format(ROT_A_PIN, ROT_B_PIN))
@@ -26,6 +27,9 @@ def main():
     try:
         while True:
             a, b = read_state()
+
+            if GPIO.input(PUSH_PIN):
+                print("PUSH")
 
             # Only react when A changes (this is our "event")
             if a != last_a:
@@ -40,7 +44,7 @@ def main():
 
                 last_a, last_b = a, b
 
-            time.sleep(0.001)  # small delay to avoid hammering CPU
+            time.sleep(0.01)  # small delay to avoid hammering CPU
 
     except KeyboardInterrupt:
         pass
